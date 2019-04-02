1bdba8ee058f38f25a0fa8bb465c6dee15fc0b63
User_Raum_174_25 <User_25@GeSa.local>
2019-04-02 15:40:19 +0200

    Adding Event Manager

---
+++ Pong Spiel/Assets/Interface/SceneController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneController : MonoBehaviour
+{
+    // +++ Life Cycle +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    void Start()
+    {
+        Debug.Log("StartCalled");
+        NvpEventController.SubscribeToEvent(NvpGameEvents.OnMainStarted, OnMainStarted);
+    }
+
+
+    void OnDisable()
+    {
+        Debug.Log("DisableCalled");
+        NvpEventController.UnsubscribeFromEvent(NvpGameEvents.OnMainStarted, OnMainStarted);
+    }
+
+
+    // +++ Event Handler ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    private void OnMainStarted(object sender, object eventargs)
+    {
+        Debug.Log("OnMainStart");
+
+    }
+}
---
+++ Pong Spiel/Assets/Scripts/Game Manager/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Threading.Tasks;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    async void Start ()
+    {
+        await Task.Delay(TimeSpan.FromSeconds(1));
+        NvpEventController.InvokeEvent(NvpGameEvents.OnMainStarted, this, 42);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
